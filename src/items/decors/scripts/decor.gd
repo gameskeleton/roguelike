d55dc2090c6e36aa2066089745b9a092dc227ab3
JonathanPicques <jonathan.picques@gmail.com>
2023-03-26 14:44:36 +0200

    feat(decors): add debris on roll

--- src/items/decors/scripts/decor.gd
+++ src/items/decors/scripts/decor.gd
@@ -1,5 +1,10 @@
 extends Node2D
 class_name RkDecor
 
+const debris_scene := preload("res://src/items/decors/debris.tscn")
+
 func _roll(_player: RkPlayer):
+	var debris := debris_scene.instantiate()
+	debris.position = position
+	get_parent().add_child(debris)
 	queue_free()
