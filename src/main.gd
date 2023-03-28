c5598c0b8d7fed39bc083738268017a158e2c3f0
JonathanPicques <jonathan.picques@gmail.com>
2023-03-28 04:18:24 +0200

    feat(dungeon): add dungeon generator

--- src/main.gd
+++ src/main.gd
@@ -3,10 +3,24 @@ class_name RkMain
 
 const ROOM_WIDTH := 512.0
 const ROOM_HEIGHT := 288.0
+const ROOMS_DIRECTORY = "res://src/levels/rooms"
 
+@export var generate_dungeon := true
+
+enum RoomExit {
+	up = 0b0001,
+	down = 0b0010,
+	left = 0b0100,
+	right = 0b1000
+}
+
+var generator: RkDungeonGenerator = load("res://src/utilities/dungeon_generator.gd").new()
 var current_room := Vector2()
 
 func _ready():
+	if generate_dungeon:
+		_generate_dungeon()
+	_restrict_camera()
 	$Player/Camera2D.reset_smoothing()
 
 func _process(delta: float):
@@ -16,7 +30,57 @@ func _process(delta: float):
 	# room camera
 	current_room.x = floor($Player.position.x / ROOM_WIDTH)
 	current_room.y = floor($Player.position.y / ROOM_HEIGHT)
+	_restrict_camera()
+
+func _restrict_camera():
 	$Player/Camera2D.limit_top = current_room.y * ROOM_HEIGHT
 	$Player/Camera2D.limit_left = current_room.x * ROOM_WIDTH
 	$Player/Camera2D.limit_right = (current_room.x + 1) * ROOM_WIDTH
 	$Player/Camera2D.limit_bottom = (current_room.y + 1) * ROOM_HEIGHT
+
+func _generate_dungeon():
+	# clear
+	for room_node in $Rooms.get_children():
+		$Rooms.remove_child(room_node)
+		room_node.queue_free()
+	# load rooms
+	var rooms_dir := DirAccess.open(ROOMS_DIRECTORY)
+	var room_scenes := {}
+	for room_scene_filename in rooms_dir.get_files():
+		var room_exits := 0
+		var room_scene: PackedScene = load(ROOMS_DIRECTORY + "/" + room_scene_filename)
+		var room_scene_state := room_scene.get_state()
+		for property_index in room_scene_state.get_node_property_count(0):
+			match room_scene_state.get_node_property_name(0, property_index):
+				"exit_up": room_exits |= RoomExit.up
+				"exit_down": room_exits |= RoomExit.down
+				"exit_left": room_exits |= RoomExit.left
+				"exit_right": room_exits |= RoomExit.right
+		if room_scenes.has(room_exits):
+			room_scenes[room_exits].append(room_scene)
+		else:
+			room_scenes[room_exits] = [room_scene]
+	# generate dungeon
+	generator.rng.randomize()
+	generator.start = 45
+	var spawns: Array[Vector2] = []
+	var dungeon := generator.next()
+	for i in dungeon.size():
+		var x := (i % 10) - 1
+		var y: int = (ceil(i / 10.0)) - 1
+		var cell := dungeon[i]
+		var cell_exits := 0
+		if cell == 1:
+			if dungeon[i - 1]: cell_exits |= RoomExit.left
+			if dungeon[i + 1]: cell_exits |= RoomExit.right
+			if dungeon[i - 10]: cell_exits |= RoomExit.up
+			if dungeon[i + 10]: cell_exits |= RoomExit.down
+			if room_scenes.has(cell_exits):
+				var room_node: Node2D = (room_scenes[cell_exits] as Array[PackedScene]).pick_random().instantiate()
+				spawns.push_back(Vector2(x, y))
+				room_node.position.x = x * ROOM_WIDTH
+				room_node.position.y = y * ROOM_HEIGHT
+				$Rooms.add_child(room_node)
+	# position player
+	current_room = spawns.pick_random()
+	$Player.position = Vector2(current_room.x * ROOM_WIDTH + ROOM_WIDTH / 2, current_room.y * ROOM_HEIGHT + ROOM_HEIGHT / 2)
---
+++ src/utilities/dungeon_generator.gd
@@ -0,0 +1,57 @@
+class_name RkDungeonGenerator
+
+var rng := RandomNumberGenerator.new()
+var start := 45 # x: 5, y: 4
+var min_rooms := 20
+var max_rooms := 25
+
+var end_rooms: Array[int] = []
+var _floorplan: Array[int] = []
+var _cell_queue: Array[int] = []
+var _floor_plan_count := 0
+
+func next() -> Array[int]:
+	while true:
+		if _cell_queue.size() > 0:
+			var i: int = _cell_queue.pop_back()
+			var x: int = i % 10
+			var created := false
+			if x > 1: created = created || _visit_cell(i - 1)
+			if x < 9: created = created || _visit_cell(i + 1)
+			if i > 20: created = created || _visit_cell(i - 10)
+			if i < 70: created = created || _visit_cell(i + 10)
+			if not created:
+				end_rooms.push_back(i)
+		else:
+			if _floor_plan_count < min_rooms:
+				_reset()
+			else:
+				return _floorplan.duplicate()
+	return []
+
+func _reset():
+	end_rooms.clear()
+	_cell_queue.clear()
+	_floorplan.clear()
+	_floorplan.resize(100)
+	_floor_plan_count = 0
+	for i in 100:
+		_floorplan[i] = 0
+	_visit_cell(start)
+
+func _visit_cell(i: int):
+	if _floorplan[i] == 1:
+		return false
+	if _floor_plan_count >= max_rooms:
+		return false
+	if _neighbour_count(i) > 1:
+		return false
+	if rng.randf() < 0.5 && i != start:
+		return false
+	_floorplan[i] = 1
+	_cell_queue.push_back(i)
+	_floor_plan_count += 1
+	return true
+
+func _neighbour_count(i: int):
+	return _floorplan[i - 10] + _floorplan[i - 1] + _floorplan[i + 1] + _floorplan[i + 10]
