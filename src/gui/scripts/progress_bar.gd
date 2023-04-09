b7f9eba7d919f066b10a9eb80873fbeaa8561498
JonathanPicques <jonathan.picques@gmail.com>
2023-04-09 22:15:09 +0200

    feat(gui): add margin around progress bar

--- src/gui/scripts/progress_bar.gd
+++ src/gui/scripts/progress_bar.gd
@@ -8,12 +8,26 @@ class_name RkGuiProgressBar
 	set(value):
 		color = value
 		_update_colors()
-@export var bg_color := Color.WHITE :
+@export var margin_color := Color.GOLD :
 	get:
-		return bg_color
+		return margin_color
 	set(value):
-		bg_color = value
+		margin_color = value
 		_update_colors()
+@export var background_color := Color.WHITE :
+	get:
+		return background_color
+	set(value):
+		background_color = value
+		_update_colors()
+
+@export var margin := Vector2.ONE :
+	get:
+		return margin
+	set(value):
+		margin = value
+		_update_progress()
+
 @export var progress := 1.0 :
 	get:
 		return progress
@@ -28,13 +42,17 @@ func _ready():
 
 # @impure
 func _update_colors():
-	$Bg.color = bg_color
-	$Progress.color = color
+	$Margin.color = margin_color
+	$Background.color = background_color
+	$ProgressBar.color = color
 
 # @impure
 func _update_progress():
 	var rect := get_rect()
-	$Progress.size = Vector2(lerp(0.0, rect.size.x, progress), rect.size.y)
+	$Margin.size = rect.size + margin * 2.0
+	$Margin.position = -margin
+	$Background.size = rect.size
+	$ProgressBar.size = Vector2(lerp(0.0, rect.size.x, progress), rect.size.y)
 
 # @signal
 # @impure
--- src/items/enemies/scripts/orb.gd
+++ src/items/enemies/scripts/orb.gd
@@ -19,7 +19,7 @@ const EXPULSE_STRENGTH := 10.0
 @onready var life_points: RkLifePoints = $LifePoints
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var life_points_progress_bar: RkGuiProgressBar = $StaminaBg/ProgressBar
+@onready var life_points_meter: RkGuiProgressBar = $LifePointsMeter
 
 var _timer := 0.0
 var _state := State.idle
@@ -99,7 +99,7 @@ func _on_life_points_damage_taken(_damage: float, new_life_points: float, source
 	_expulse = (global_position - source.global_position).normalized() * EXPULSE_STRENGTH
 	_expulse_alpha = 1.0
 	animation_player.play("hit")
-	life_points_progress_bar.progress = life_points.get_ratio()
+	life_points_meter.progress = life_points.get_ratio()
 	if new_life_points <= 0.0:
 		for i in 10:
 			var pickup_node: RkPickupExperience = pickup_experience_scene.instantiate()
