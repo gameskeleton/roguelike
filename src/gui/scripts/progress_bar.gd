929bdb8020b755d05776fb303ca5d3b79c5e7b77
JonathanPicques <jonathan.picques@gmail.com>
2023-04-10 16:59:13 +0200

    feat(gui): add lag in progress bar

--- src/gui/scripts/progress_bar.gd
+++ src/gui/scripts/progress_bar.gd
@@ -8,6 +8,12 @@ class_name RkGuiProgressBar
 	set(value):
 		color = value
 		_update_colors()
+@export var lag_color := Color.DARK_GREEN :
+	get:
+		return lag_color
+	set(value):
+		lag_color = value
+		_update_colors()
 @export var margin_color := Color.GOLD :
 	get:
 		return margin_color
@@ -34,26 +40,56 @@ class_name RkGuiProgressBar
 		progress = value
 		_update_progress()
 
+@export var lag := true
+@export var lag_delay := 0.35
+@export var lag_speed := 30.0
+
+var _lag_delay := lag_delay
+
 # @impure
 func _ready():
+	_update_lag()
+	_update_size()
 	_update_colors()
 	_update_progress()
+
+# @impure
+func _process(delta: float):
+	if $ProgressBarLag.size.x <= $ProgressBarCurrent.size.x:
+		_lag_delay = lag_delay
+		$ProgressBarLag.size.x = $ProgressBarCurrent.size.x
+	if _lag_delay > 0.0:
+		_lag_delay -= delta
+	if _lag_delay <= 0.0:
+		$ProgressBarLag.size.x = move_toward($ProgressBarLag.size.x, $ProgressBarCurrent.size.x, delta * lag_speed)
+
+# @impure
+func _update_lag():
+	set_process(lag)
+	$ProgressBarLag.visible = lag
+
+# @impure
+func _update_size():
+	var rect := get_rect()
+	$Margin.size = rect.size + margin * 2.0
+	$Margin.position = -margin
+	$Background.size = rect.size
+	$ProgressBarLag.size = rect.size
+	$ProgressBarCurrent.size = Vector2(lerp(0.0, rect.size.x, progress), rect.size.y)
 
 # @impure
 func _update_colors():
 	$Margin.color = margin_color
 	$Background.color = background_color
-	$ProgressBar.color = color
+	$ProgressBarLag.color = lag_color
+	$ProgressBarCurrent.color = color
 
 # @impure
 func _update_progress():
 	var rect := get_rect()
-	$Margin.size = rect.size + margin * 2.0
-	$Margin.position = -margin
-	$Background.size = rect.size
-	$ProgressBar.size = Vector2(lerp(0.0, rect.size.x, progress), rect.size.y)
+	$ProgressBarCurrent.size = Vector2(lerp(0.0, rect.size.x, progress), rect.size.y)
 
 # @signal
 # @impure
 func _on_resized():
-	_update_progress()
+	_update_size()
