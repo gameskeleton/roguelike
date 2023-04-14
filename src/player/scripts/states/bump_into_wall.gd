14f140cdc18c6f75d3ff7c0a1aab73231f2650be
JonathanPicques <jonathan.picques@gmail.com>
2023-04-14 17:20:14 +0200

    feat(player): split bump_into_wall

--- src/player/scripts/states/bump_into_wall.gd
+++ src/player/scripts/states/bump_into_wall.gd
@@ -1,18 +1,33 @@
 extends RkStateMachineState
 
+enum State { fall, hit_floor, get_up }
+
+var _state := State.fall
 var _animation_initial_speed_scale := 1.0
 
 func start_state():
+	_state = State.fall
 	_animation_initial_speed_scale = player_node.animation_player.speed_scale
 	player_node.handle_roll(player_node.direction * player_node.ROLL_BUMP_STRENGTH)
-	player_node.play_animation("death")
+	player_node.play_animation("bump_into_wall_fall")
 	player_node.animation_player.speed_scale = 1.8
 
 func process_state(delta: float):
 	player_node.handle_gravity(delta, player_node.GRAVITY_MAX_SPEED, player_node.GRAVITY_ACCELERATION)
 	player_node.handle_deceleration_move(delta, player_node.ROLL_DECELERATION)
-	if player_node.is_stopped() and player_node.is_animation_finished():
-		return player_node.fsm.state_nodes.stand
+	match _state:
+		State.fall:
+			if player_node.is_on_floor() and player_node.is_animation_finished():
+				_state = State.hit_floor
+				player_node.play_animation("bump_into_wall_hit_floor")
+		State.hit_floor:
+			if player_node.is_stopped() and player_node.is_animation_finished():
+				_state = State.get_up
+				player_node.play_animation("get_up")
+				player_node.animation_player.speed_scale = 1.2
+		State.get_up:
+			if player_node.is_stopped() and player_node.is_animation_finished():
+				return player_node.fsm.state_nodes.stand
 
 func finish_state():
 	player_node.animation_player.speed_scale = _animation_initial_speed_scale
