5121d3ca845b7a17630d90560886a7e6d0977c03
JonathanPicques <jonathan.picques@gmail.com>
2023-06-29 21:27:09 +0200

    feat(decor): spawn coin from sensible position

--- src/objects/decors/scripts/decor.gd
+++ src/objects/decors/scripts/decor.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 @export var debris_scene: PackedScene
+@export var coin_spawn_position: Node2D
 
 # @signal
 # @impure
 func _on_life_points_damage_taken(_damage: float, _source: Node, _instigator: Node):
-	RkObjectSpawner.spawn_coin(self, global_position).fly()
+	RkObjectSpawner.spawn_coin(self, (self if not coin_spawn_position else coin_spawn_position).global_position).fly()
 	if debris_scene:
 		var debris: Node2D = debris_scene.instantiate()
 		debris.position = position
