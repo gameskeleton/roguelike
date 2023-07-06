27b3a03f7a4173de2f5fb6f684f4f0937d1fdfe0
JonathanPicques <jonathan.picques@gmail.com>
2023-07-06 23:06:27 +0200

    chore(godot): upgrade to 4.1

--- src/objects/decors/scripts/debris.gd
+++ src/objects/decors/scripts/debris.gd
@@ -3,18 +3,17 @@ extends Node2D
 const FADE_OUT_SPEED := 1.0
 
 @export var force := 200.0
-@export var pieces: Array[NodePath]
-@onready var debris_pieces := pieces.map(get_node) # TODO: remove me when godot 4.1 lands (https://github.com/godotengine/godot/pull/73256)
+@export var pieces: Array[RigidBody2D]
 
 var _fade_out := 1.0
 var _start_fade_out := 2.0
 
 func _ready():
-	for piece in debris_pieces:
+	for piece in pieces:
 		piece.angular_velocity = randf_range(5.0, 10.0)
 		piece.apply_central_impulse(Vector2.UP.rotated(deg_to_rad(randf_range(-25.0, +25.0))) * force)
 
-func _process(delta):
+func _process(delta: float):
 	if _start_fade_out > 0:
 		_start_fade_out -= delta * FADE_OUT_SPEED
 		return
