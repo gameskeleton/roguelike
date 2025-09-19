f47d1915d88ab95c7433305f0f8d3b706d13c0b8
JonathanPicques <jonathan.picques@gmail.com>
2025-09-19 23:33:36 +0200

    fix: invincible frame

--- src/systems/scripts/life_points.gd
+++ src/systems/scripts/life_points.gd
@@ -19,7 +19,7 @@ const NO_DAMAGE := -1.0
 
 @export_group("Invincible")
 @export var invincible := 0
-@export var invincibility_delay := 0.0
+@export var prevent_take_damage_until_next_frame := 0.0
 
 @export_group("Damage multipliers", "damage_multiplier")
 @export var damage_multiplier_fire := 1.0
@@ -34,16 +34,16 @@ var last_damage_type := DmgType.none
 var last_damage_source: Node
 var last_damage_instigator: Node
 
-# _process reduces the invincibility delay.
+# _process makes sure to clear the prevent_take_damage_until_next_frame flag.
 # @impure
-func _process(delta: float):
-	invincibility_delay = maxf(0.0, invincibility_delay - delta)
+func _process(_delta: float):
+	prevent_take_damage_until_next_frame = false
 
 # take_damage reduces the life points by the amount of damage with respect to damage type multipliers.
 # @impure
 func take_damage(damage: float, damage_type := DmgType.none, from_source: Node = null, from_instigator: Node = null) -> float:
 	assert(damage >= 0.0, "damage must be positive")
-	if invincible > 0 or invincibility_delay > 0.0:
+	if invincible > 0 or prevent_take_damage_until_next_frame:
 		return NO_DAMAGE
 	var damage_multiplier := 1.0
 	if RkLifePointsSystem.is_damage_type(damage_type, RkLifePointsSystem.DmgType.fire): damage_multiplier *= damage_multiplier_fire
@@ -53,10 +53,13 @@ func take_damage(damage: float, damage_type := DmgType.none, from_source: Node =
 	if RkLifePointsSystem.is_damage_type(damage_type, RkLifePointsSystem.DmgType.physical): damage_multiplier *= damage_multiplier_physical
 	var scaled_damage := damage * damage_multiplier
 	var life_points_previous := life_points.value
+	if scaled_damage <= 0.0:
+		return NO_DAMAGE
 	last_damage = scaled_damage
 	last_damage_type = damage_type
 	last_damage_source = from_source
 	last_damage_instigator = from_instigator
+	prevent_take_damage_until_next_frame = true
 	damage_taken.emit(life_points.sub(scaled_damage), from_source, from_instigator)
 	life_points_changed.emit(life_points.value, life_points.ratio, life_points_previous)
 	return scaled_damage
@@ -65,11 +68,6 @@ func take_damage(damage: float, damage_type := DmgType.none, from_source: Node =
 # @pure
 func has_lethal_damage() -> bool:
 	return life_points.value <= 0.0
-
-# set_invincibility_delay refills the invincibility delay towards the given value.
-# @impure
-func set_invincibility_delay(delay: float):
-	invincibility_delay = maxf(delay, invincibility_delay)
 
 # is_damage_type returns true if the given individual_damage_type is included in the given damage_type.
 # @pure
