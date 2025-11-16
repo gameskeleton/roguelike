dcff63fd43b72f706251b644c3657d583d5728d1
JonathanPicques <jonathan.picques@gmail.com>
2025-11-16 14:41:45 +0100

    fix: check is player is able to jump/wall_slide before transitioning

--- src/player/scripts/states/wall_hang.gd
+++ src/player/scripts/states/wall_hang.gd
@@ -11,6 +11,7 @@ func start_state():
 		- Vector2(player_node.direction * absf(player_node.hand_marker.position.x), player_node.hand_marker.position.y)
 	player_node.play_animation(&"wall_hang")
 	player_node.set_wall_hang_raycast_active(true)
+	player_node.set_wall_slide_raycast_active(true)
 	player_node.set_wall_climb_shapecast_active(true)
 
 func process_state(_delta: float):
@@ -20,14 +21,15 @@ func process_state(_delta: float):
 	if player_node.input_down.is_pressed():
 		player_node.input_down.consume()
 		player_node.disable_wall_hang_timeout = player_node.WALL_HANG_DROP_TIMEOUT
-		if player_node.has_same_direction(player_node.direction, player_node.input_velocity.x):
+		if player_node.has_same_direction(player_node.direction, player_node.input_velocity.x) and player_node.is_able_to_wall_slide():
 			return player_node.fsm.state_nodes.wall_slide
 		return player_node.fsm.state_nodes.fall
-	if player_node.input_jump.is_pressed() and player_node.has_invert_direction(player_node.direction, player_node.input_velocity.x):
+	if player_node.input_jump.is_pressed() and player_node.is_able_to_jump() and player_node.has_invert_direction(player_node.direction, player_node.input_velocity.x):
 		player_node.input_jump.consume()
 		player_node.velocity.x = player_node.direction * player_node.WALL_HANG_JUMP_EXPULSE_STRENGTH
 		return player_node.fsm.state_nodes.jump
 
 func finish_state():
 	player_node.set_wall_hang_raycast_active(false)
+	player_node.set_wall_slide_raycast_active(false)
 	player_node.set_wall_climb_shapecast_active(false)
