eb0af479aaefdc4260a71fb6864374f87c498388
JonathanPicques <jonathan.picques@gmail.com>
2026-04-19 23:45:56 +0200

    fix: correct gravity application in projectile movement

--- src/items/projectiles/scripts/projectile_arrow.gd
+++ src/items/projectiles/scripts/projectile_arrow.gd
@@ -23,12 +23,14 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if stuck:
 		return
-	velocity.y += gravity * delta
 	rotation = velocity.angle()
+	velocity += -up_direction * gravity * delta
 	var collision := move_and_collide(velocity * delta)
 	if collision:
 		var normal := collision.get_normal()
 		var tangent := normal.rotated(PI * 0.5)
+		if tangent.dot(up_direction) < 0:
+			tangent = -tangent
 		var angle_impact := velocity.angle_to(tangent)
 		var angle_to_line := minf(absf(angle_impact), PI - absf(angle_impact))
 		if angle_to_line < deg_to_rad(bounce_max_angle) and velocity.length() > bounce_min_velocity:
